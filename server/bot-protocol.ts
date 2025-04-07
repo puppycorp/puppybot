b576264e06d6f1fbd1a80918a0dc5def8d1406b1
Jaska <pusanteemu@gmail.com>
2025-04-07 08:59:21 +0300

    start protocol impl

--- server/bot-protocol.ts
+++ server/bot-protocol.ts
@@ -1,5 +1,64 @@
 import type { MsgFromBot, MsgToBot } from "./types"
 
+enum CommandType {
+	SendInstructions = 1,
+	StopAll = 2,
+	ReplaceBlock = 3,
+	PauseAll = 4,
+	ResumeAll = 5,
+}
+
+enum InstructionType {
+	Sleep = 1,
+	Stop = 2,
+	DoUntil = 3,
+	Drive = 4,
+}
+
+enum Operator {
+	Equal = 1,
+	NotEqual = 2,
+	GreaterThan = 3,
+	LessThan = 4,
+	GreaterThanOrEqual = 5,
+	LessThanOrEqual = 6,
+	And = 7,
+	Or = 8,
+	Forever = 9
+}
+
+type ConditionFrame = {
+	targetId: number
+	fieldId: number
+	operator: Operator
+	value: number
+}
+
+type DoUntilCondition = {
+	type: InstructionType.DoUntil
+	targetId: number
+	instruction: InstructionType.Drive,
+}
+
+type StopAll = {
+	type: CommandType.StopAll
+}
+
+type Command = StopAll
+
+const header = (cmd: Command) => {
+
+}
+
+type Instruction = {
+	type: InstructionType
+	args: any[]
+}
+
+const block = (instructions: Instruction[]) => {
+	
+}
+
 export const encodeBotMsg = (msg: MsgToBot): Buffer => {
 	const buffer = Buffer.alloc(4 + msg.length)
 	buffer.writeUInt32BE(msg.length, 0)
