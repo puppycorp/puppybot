429b338a68f38bc34851f1355acaace0484e0ca7
Jaska <pusanteemu@gmail.com>
2025-06-14 17:19:04 +0300

    add prepare-agent.sh

--- prepare-agent.sh
+++ prepare-agent.sh
@@ -1,2 +1,11 @@
 sudo apt update
-sudo apt install -y clang-format
+sudo apt-get update
+sudo apt-get install -y git wget flex bison gperf python3 python3-pip \
+     python3-venv cmake ninja-build ccache libffi-dev libssl-dev \
+     dfu-util libusb-1.0-0 clang-format
+
+mkdir -p ~/esp && cd ~/esp
+git clone -b v5.4.1 --recursive https://github.com/espressif/esp-idf.git
+cd ~/esp/esp-idf
+./install.sh esp32
+. $HOME/esp/esp-idf/export.sh
